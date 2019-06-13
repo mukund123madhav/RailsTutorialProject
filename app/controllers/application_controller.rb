08f6a53a2c68eb3e2feda329a913a5619f608bd4
Mukund Madhav <mukund.madhav@box8.in>
2019-06-13 16:51:44 +0530

    Initialize Repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+  	render html: "¡hola!"
+  end
+  def goodbye
+  	render html: "Goodbye my friend"
+  end
+end
